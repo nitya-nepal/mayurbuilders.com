6a48cf95801498b5bc4602d4076b42507f53eda8
saileshkasaju <leosailesh@gmail.com>
2019-03-14 16:14:25 +0545

    add team url

---
+++ src/pages/team/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const TeamsPage = () => (
+  <Layout>
+    <section className="section">
+      <div>Teams</div>
+    </section>
+  </Layout>
+);
+
+export default TeamsPage;
